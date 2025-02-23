e837f6f1c151f6fdf070349cee46356913ff508a
Phaham Khan <72141945+Phaham@users.noreply.github.com>
2025-02-23 16:21:24 +0530

    Update KPI.js

--- models/KPI.js
+++ models/KPI.js
@@ -65,14 +65,18 @@ const KPISchema = new Schema(
       currency: "USD",
       get: (v) => v / 100,
     },
-    expensesByCategory: {
-      type: Map,
-      of: {
-        type: mongoose.Types.Currency,
-        currency: "USD",
-        get: (v) => v / 100,
-      },
-    },
+    // expensesByCategory: {
+    //   type: Map,
+    //   of: {
+    //     type: mongoose.Types.Currency,
+    //     currency: "USD",
+    //     get: (v) => v / 100,
+    //   },
+    // },
+   expensesByCategory: {
+    type: Map,
+    of: Number
+   },
     monthlyData: [monthSchema],
     dailyData: [daySchema],
   },
